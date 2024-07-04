cda0b9004951ffa2d492f098c59332dd1235a971
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-04 22:48:01 +0200

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Buhle Radzilani on 2024/07/04.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
