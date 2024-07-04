a1c2aa9bec47da4ae97e0e4722f2f025f0c37523
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-04 22:58:40 +0200

    setting up the Folders and also the home view which is going to be the main folder of our application.

---
+++ UberClone/Core/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  UberClone
+//
+//  Created by Buhle Radzilani on 2024/07/04.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
