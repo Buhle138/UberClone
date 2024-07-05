bcac6925ad940a90f113ccd6d29f9fc61d63bd81
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-05 11:21:08 +0200

    creating the Location search folders so that we can add those features

---
+++ UberClone/Core/LocationSearch/Views/LocationSearchActivationView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationSearchActivationView.swift
+//  UberClone
+//
+//  Created by Buhle Radzilani on 2024/07/05.
+//
+
+import SwiftUI
+
+struct LocationSearchActivationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationSearchActivationView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchActivationView()
+    }
+}
