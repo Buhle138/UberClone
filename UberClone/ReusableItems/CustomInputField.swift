dc7369e95cdf5984401345ee59aae5cf4211d9c9
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-11 16:08:26 +0200

    creating re usable components for our UI views.

---
+++ UberClone/ReusableItems/CustomInputField.swift
@@ -0,0 +1,20 @@
+//
+//  CustomInputField.swift
+//  UberClone
+//
+//  Created by Buhle Radzilani on 2024/07/11.
+//
+
+import SwiftUI
+
+struct CustomInputField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomInputField_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomInputField()
+    }
+}
