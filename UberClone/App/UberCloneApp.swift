4cec1b9282436ccb90c11747359d1cb83c682bf3
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-11 21:15:53 +0200

    Setting up the firebase for authentication and also the plist.

--- UberClone/App/UberCloneApp.swift
+++ UberClone/App/UberCloneApp.swift
@@ -6,10 +6,20 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct UberCloneApp: App {
     
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     //WE ARE USING THIS VARIABLE AND INJECTING IT INTO THE ENVIRONMENT OBJECT SO THAT WE CAN INITIATE/CALL IT'S CONSTRUCTOR ONCE AND USE IT THROUGHOUT THE ENTIRE APPLICATION AS AN ENVIRONMENT OBJECT WRAPPER.
     //ONCE WE CALL THIS LocationSearchViewModel AS AN ENVIRONMENT OBJECT THERE IS NO NEED FOR US TO CALL IT'S CONSTRUCTOR ANY MORE THROUGHOUT THE ENTIRE APPLICATION.
