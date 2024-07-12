255259f82e387ed396bf745706f2829f39f21c3e
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-12 10:06:46 +0200

    creating the User model struct that will help us upload user data to the cloud firestorm.

---
+++ UberClone/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  UberClone
+//
+//  Created by Buhle Radzilani on 2024/07/12.
+//
+
+import Foundation
+
+
+struct User: Codable {
+    
+    let fullname: String
+    let email: String
+    let uid: String
+    
+}
