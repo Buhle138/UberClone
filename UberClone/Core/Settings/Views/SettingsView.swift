3611d137c5ac99fa931e32984357b4a402668bfb
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-15 09:31:39 +0200

    Creating the settings view.

---
+++ UberClone/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  UberClone
+//
+//  Created by Buhle Radzilani on 2024/07/15.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
