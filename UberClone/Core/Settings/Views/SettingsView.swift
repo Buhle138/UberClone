cc502f3859441a7f338957741c30fe68906cea41
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-16 16:57:05 +0200

    creating the profile of the user inside the settings view.

--- UberClone/Core/Settings/Views/SettingsView.swift
+++ UberClone/Core/Settings/Views/SettingsView.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List {
+                Section {
+                    //User info header
+                    HStack {
+                        Image("male-profile-photo")
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .frame(width: 64, height: 64)
+                        
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("Buhle")
+                                .font(.system(size: 16, weight: .semibold))
+                            
+                            Text("buhle@gmail.com")
+                                .font(.system(size: 14))
+                                .accentColor(Color.theme.primaryTextColor)
+                                .opacity(0.77)
+                        }
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                        
+                    }
+                }
+                
+                Section("Favorites") {
+                    
+                }
+                
+                Section("Settings") {
+                    
+                }
+                
+                Section("Account") {
+                    
+                }
+            }
+        }
     }
 }
 
